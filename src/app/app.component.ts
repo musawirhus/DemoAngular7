95c638de6ec73d8f0e3367080b4a1e8890da134b
MUSAWIR HUSSAIN <MUSAWIRHUSSAIN@ISB.MTBC.COM>
2021-01-11 09:07:34 +0500

    anualr 6 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriesService } from './northwind.service';
+
+import { State } from '@progress/kendo-data-query';
+
+import {
+    GridDataResult,
+    DataStateChangeEvent
+} from '@progress/kendo-angular-grid';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `
+       <kendo-grid
+          [data]="view | async"
+          [loading]="view.loading"
+          [pageSize]="state.take"
+          [skip]="state.skip"
+          [sort]="state.sort"
+          [sortable]="true"
+          [pageable]="true"
+          scrollable="none"
+          (dataStateChange)="dataStateChange($event)"
+        >
+        <kendo-grid-column field="CategoryID" width="130"></kendo-grid-column>
+        <kendo-grid-column field="CategoryName" width="200"></kendo-grid-column>
+        <kendo-grid-column field="Description" [sortable]="false">
+        </kendo-grid-column>
+      </kendo-grid>
+    `
 })
 export class AppComponent {
-  title = 'Angular6Project';
+    public view: Observable<GridDataResult>;
+    public state: State = {
+        skip: 0,
+        take: 5
+    };
+
+    constructor(private service: CategoriesService) {
+        this.view = service;
+        this.service.query(this.state);
+    }
+
+    public dataStateChange(state: DataStateChangeEvent): void {
+        this.state = state;
+        this.service.query(state);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
+import { CategoriesService } from './northwind.service';
+
+enableProdMode();
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, BrowserAnimationsModule, GridModule, HttpClientModule ],
+  declarations: [ AppComponent ],
+  providers: [ CategoriesService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
